cb5cb8e7e6faecb83dc2a267a5a67a21b095614f
Erik Nomitch <enomitch@gmail.com>
2025-05-28 17:21:18 -0500

    feat: add utility module and import it in main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from ipdb import set_trace as st
 import click
+import src.utility
 
 def main():
     print("Hello from firecrawl-skills!")
---
+++ src/utility.py
@@ -0,0 +1,4 @@
+# This is the new utility.py file.
+# Add your utility functions here.
+
+print("utility.py loaded")
