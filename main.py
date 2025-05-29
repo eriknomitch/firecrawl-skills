e2f12c46a4247db22de976357f67206d343825b8
Erik Nomitch <enomitch@gmail.com>
2025-05-29 08:30:41 -0500

    feat: add pyperclip dependency and update run script argument handling

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from ipdb import set_trace as st
 import click
 from src.utility import get_firecrawl_api_key
-from src.firecrawl import run_examples
+from src.firecrawl import run_examples, app
 from dotenv import load_dotenv
 import os
 
@@ -49,6 +49,7 @@ def help_command(ctx):
     """Shows the help message."""
     # The parent context is the main command group
     click.echo(ctx.parent.get_help())
+
 
 main.add_command(help_command, name="h")
 
